d0e28b07835d6bcf68b1af66fa0d9f5164950478
Thomeeh <57054428+Thomeeh@users.noreply.github.com>
2025-09-04 18:09:42 -0300

    Add files via upload
    Diccionario de preguntas

---
+++ Diccionario de preguntas.py
@@ -0,0 +1,17 @@
+    preguntas = {
+    1: {
+        "enunciado": "Capital de Francia",
+        "opciones": ["Paris", "Roma", "Berlin"],
+        "respuesta": "Paris"
+    },
+    2: {
+        "enunciado": "5 + 7 = ?",
+        "opciones": ["10", "12", "15"],
+        "respuesta": "12"
+    },
+    3: {
+        "enunciado": "Vuelo sin alas y lloro sin ojos, ¿qué soy?",
+        "opciones": ["Un rio", "El viento", "Una nube"],
+        "respuesta": "El viento"
+    }
+}
